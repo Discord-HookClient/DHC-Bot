731ade824005d42c3b69014c82f1fe124b60b955
Gideon Lingle <gideon.lingle1@gmail.com>
2019-02-11 12:31:17 -0600

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,23 @@
 
 module.exports.run = (bot, message, args) => {
-
-}
+  const member = message.mentions.members.first()
+  const reason = args.slice(1).join(" ") || "no reason provided."
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid permissions.");
+  if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`I can't ban people. Make sure that I have the Ban Members permission.`);
+  
+  member.ban(reason)
+  .then(member => {
+    const em = new embed()
+    .setTitle("Modbot Banning Utility")
+    .setDescription(`👢 I put my digital foot in ${member.displayName}'s digital ass.`)
+    .setFooter(`${member.displayName} got a foot to the ass.`)
+    .setTimestamp()
+    .setColor("RANDOM")
+    message.channel.send({embed: em})
+  })
+  .catch(err => {
+    message.channel.send(`I couldn't ban ${member.displayName}...`)
+  })
 
 module.exports.help = {
   perm: "owner",
